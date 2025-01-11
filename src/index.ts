490a6ec41b6777e24c2b0030dde83aa585bcb7d2
miyou2024 <love@miyou.top>
2025-01-11 10:44:24 +0800

    wip: 代表进行中

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 /**
  * test
+ * git-commit
+ *  feat
+ *  fix
+ *  docs
+ *  dx
+ *  style
+ *  refactor
+ *  perf
+ *  test
+ *  workflow
+ *  build
+ *  ci
+ *  chore
+ *  types
+ *  wip
+ *  release
  */
 export const one = 1
 export const two = 2
